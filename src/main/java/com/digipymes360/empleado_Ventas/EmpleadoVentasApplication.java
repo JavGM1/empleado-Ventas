129c4a43b6732ab8e1b3aeca34188d442fff9a34
JavoT1 <javierignaciogalarcem@gmail.com>
2025-05-19 00:28:29 -0400

    first commit

---
+++ src/main/java/com/digipymes360/empleado_Ventas/EmpleadoVentasApplication.java
@@ -0,0 +1,13 @@
+package com.digipymes360.empleado_Ventas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpleadoVentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpleadoVentasApplication.class, args);
+	}
+
+}
