995e881823c9675bd0a1f50c279fbb6a351ed9ad
JavoT1 <javierignaciogalarcem@gmail.com>
2025-05-20 12:10:19 -0400

    Versión con "Factura"

---
+++ src/main/java/com/digipymes360/empleado_Ventas/model/Factura.java
@@ -0,0 +1,22 @@
+package com.digipymes360.empleado_Ventas.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class Factura {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "factura_seq")
+    @SequenceGenerator(name = "factura_seq", sequenceName = "FACTURA_SEQ", allocationSize = 1)
+    private Long idFactura;
+
+    private Long idVenta;
+    private LocalDateTime fechaEmision;
+    private String archivoPdf;
+}
